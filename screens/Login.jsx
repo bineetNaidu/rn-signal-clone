d80f4f37f4be1c47b5a647f59d81cde4a5aa702f
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-23 16:04:32 +0530

    🚀 add styles && UI elements to Login Screen

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import { Button, Input, Image } from 'react-native-elements';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = () => {};
+
   return (
-    <View>
-      <Text>The Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Image
+        source={{
+          uri: 'https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png',
+        }}
+        style={styles.image}
+      />
+      <View style={styles.input}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login!" />
+      <Button containerStyle={styles.button} type="outline" title="Register?" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  input: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
 
 export default Login;
