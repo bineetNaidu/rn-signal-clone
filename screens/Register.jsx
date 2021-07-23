930ef594d79453dfeeb2ce6e886112fa8b5dafc5
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-23 16:49:45 +0530

    🔥 styles up Register Page w/ UI's

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -1,14 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  const register = () => {};
+
   return (
-    <View>
-      <Text>I AM the Register Screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Text h3 style={styles.header}>
+        Create a Signal Account
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Full Name"
+          autoFocus
+          type="text"
+          value={name}
+          onChangeText={(txt) => setName(txt)}
+        />
+        <Input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Input
+          placeholder="Avatar (optional!)"
+          type="text"
+          value={avatar}
+          onChangeText={(text) => setAvatar(text)}
+          onSubmitEditing={register}
+        />
+      </View>
+
+      <Button
+        title="Register"
+        onPress={register}
+        raised
+        containerStyle={styles.button}
+      />
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  inputContainer: {
+    width: 300,
+  },
+  header: {
+    marginBottom: 50,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
 
 export default Register;
