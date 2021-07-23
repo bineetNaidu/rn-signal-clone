419e6a43bbdfad92afb1a92fcd420e335e406275
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-23 15:28:42 +0530

    ✨ setting up base stack navigations

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: '#2c6BED' },
+          headerTintColor: 'white',
+          headerTitleStyle: { color: 'white' },
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Login = () => {
+  return (
+    <View>
+      <Text>The Login Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Login;
